514caaee84e3cc856ff69a739952c2a8046ec702
syeminPark <soccer0874@gmail.com>
2024-09-03 12:06:33 +0900

    Revert "login page"
    This reverts commit e04f7b28906285a205d83f8e0cc6d36f78509c00.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,36 +11,7 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
-  const [ID, setID] = useState('')
-
-//lets you run code after the component has rendered
-  useEffect(() => {
-    // Fetch the user email and token from local storage
- 
-    const user = JSON.parse(localStorage.getItem('user'))
-    console.log('eeef',user)
-  
-    // If the token/email does not exist, mark the user as logged out
-    if (!user || !user.token) {
-      setLoggedIn(false)
-      return
-    }
-  
-    // If the token exists, verify it with the auth server to see if it is valid
-    fetch('/verify', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${user.token}`
-      }
-    })
-      .then((r) => r.json())
-      .then((r) => {
-     console.log(r)
-        setLoggedIn(r.success)
-        setID(user.ID || '')
-      })
-  }, [])
+  const [email, setEmail] = useState('')
 
   return (
     <div className="App">
@@ -48,9 +19,9 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<Home ID={ID} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
+            element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
           />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setID={setID} />} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/home.js
+++ frontend/src/home.js
@@ -4,21 +4,14 @@ import { useNavigate } from 'react-router-dom'
 
 const Home = (props) => {
   //
-  const { loggedIn, ID } = props
+  const { loggedIn, email } = props
   //This hook returns a function called navigate, which can be used to programmatically navigate to 
   //different routes within the application. This function will be useful for redirecting the user to 
   //different pages, such as the login page or the home page, based on their interaction.
   const navigate = useNavigate()
 
   const onButtonClick = () => {
-    if (loggedIn) {
-      localStorage.removeItem('user')
-      props.setLoggedIn(false)
-    } else {
-      navigate('/login')
-    } 
   }
-  
 
   return (
     <div className="mainContainer">
@@ -33,7 +26,7 @@ const Home = (props) => {
           onClick={onButtonClick}
           value={loggedIn ? 'Log out' : 'Log in'}
         />
-        {loggedIn ? <div>Your ID is {ID}</div> : <div />}
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
       </div>
     </div>
   )
